de1950be9367eb41f30252cfa717a5f40c84266d
Antonio DiMaggio <antoniomdimaggio@gmail.com>
2021-08-22 21:29:03 -0700

    Added queue and node

--- tests/data_structure_tests.py
+++ tests/data_structure_tests.py
@@ -3,10 +3,12 @@ import distructures
 
 def main():
     queue = distructures.cyclicQueue(10)
+
     for i in range(100):
         queue.push(i)
-        print(queue)
-        print(queue.front(), queue.rear())
+
+    while not queue.empty():
+        print(queue.pop())
 
 
 if "__main__" == __name__:
